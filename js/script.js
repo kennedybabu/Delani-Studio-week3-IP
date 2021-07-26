62e6c89ee1fb14950ef7693eadc261b76ca19fa8
kennedy babu <kennedynjogu8@gmail.com>
2021-07-26 08:26:59 +0300

    add media query

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-console.log(5)
 
 $(document).ready(function(){
-    // alert("We are ready")
 
     //toggle the left icon
     $('.left').click(function(){
@@ -18,7 +16,7 @@ $(document).ready(function(){
         $('.middle-front-side').toggle()
     })
 
-    $('.midle').click(function(){
+    $('.middle').click(function(){
         $('.middle-back-side').toggle()
     })
 
@@ -50,4 +48,60 @@ $(document).ready(function(){
     }, function(){
         $(this).find(".over-lay-3").fadeOut(100)
     })
+
+    $(".div4").hover(function(){
+        $(this).find(".over-lay-4").fadeIn(200)
+    }, function(){
+        $(this).find(".over-lay-4").fadeOut(100)
+    })
+
+    $(".div5").hover(function(){
+        $(this).find(".over-lay-5").fadeIn(200)
+    }, function(){
+        $(this).find(".over-lay-5").fadeOut(100)
+    })
+
+    $(".div6").hover(function(){
+        $(this).find(".over-lay-6").fadeIn(200)
+    }, function(){
+        $(this).find(".over-lay-6").fadeOut(100)
+    })
+
+    $(".div7").hover(function(){
+        $(this).find(".over-lay-7").fadeIn(200)
+    }, function(){
+        $(this).find(".over-lay-7").fadeOut(100)
+    })
+
+    $(".div8").hover(function(){
+        $(this).find(".over-lay-8").fadeIn(200)
+    }, function(){
+        $(this).find(".over-lay-8").fadeOut(100)
+    })
+
+   
+
+    //submit user input 
+    // $("form").keypress(function(event){        
+    //     if(event.which === 13) {   
+    //         event.preventDefault()
+    //         $("#form").submit()
+    //         return false
+    //     }
+    // })
 })
+// let submitForm = document.getElementById("#form")
+
+// document.querySelector("form").addEventListener("keyup", function(event){
+//     let userName = document.getElementById("#name").value
+
+//     if(event.keycode === "Enter") {
+//         event.preventDefault()
+//         console.log('hello')
+//         document.getElementById("#form").submit()
+//         return false
+//     }
+// })
+
+let 
+
